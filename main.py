9deb320988410abc3459f3f9c12eb75a2255606e
MarcBogdan <marc.bogdan28@gmail.com>
2021-11-10 16:41:28 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+
+class Hello(Resource):
+    def get(self):
+        return "---"
+
+
+api.add_resource(Hello, "/test")
 
 if __name__ == "__main__":
     app.run(debug=True)
