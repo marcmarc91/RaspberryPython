9cb6b06614d2b3b302d949d54688719165efe3e3
MarcBogdan <marc.bogdan28@gmail.com>
2021-11-13 17:25:36 +0200

    update

--- steppet.py
+++ steppet.py
@@ -17,7 +17,7 @@ for pin in StepPins:
         GPIO.setup(pin,GPIO.OUT)
         GPIO.output(pin, False)
 # Define some settings
-WaitTime = 0.005
+WaitTime = 0.001
 
 # Define simple sequence
 StepCount1 = 4
